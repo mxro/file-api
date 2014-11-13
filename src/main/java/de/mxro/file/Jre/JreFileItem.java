af7c1115fbdb1e02d1d8dd9c4d0293c8feecf14a
mxro <mxro@nowhere.com>
2014-11-13 14:44:49 +1100

    Developing first implementation for FileSync Autocommit Thu Nov 13 14:44:49 EST 2014

--- src/main/java/de/mxro/file/Jre/JreFileItem.java
+++ src/main/java/de/mxro/file/Jre/JreFileItem.java
@@ -7,6 +7,36 @@ import de.mxro.file.FileItem;
 public class JreFileItem implements FileItem {
 
     public InputStream getInputStream() {
+        return null;
+    }
+
+    @Override
+    public boolean isDirectory() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean exists() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public FileItem getChild(final String childName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public FileItem assertFolder(final String folderName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setVisible(final boolean isVisible) {
+        // TODO Auto-generated method stub
 
     }
 
