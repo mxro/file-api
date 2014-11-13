bbbcb71bf86c7766dd83b3c960ae8b1cf3ac27c0
mxro <mxro@nowhere.com>
2014-11-13 14:43:41 +1100

    a

---
+++ src/main/java/de/mxro/file/Jre/JreFileItem.java
@@ -0,0 +1,13 @@
+package de.mxro.file.Jre;
+
+import java.io.InputStream;
+
+import de.mxro.file.FileItem;
+
+public class JreFileItem implements FileItem {
+
+    public InputStream getInputStream() {
+
+    }
+
+}
---
+++ src/main/java/de/mxro/file/Jre/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Classes requiring a full Java or Android runtime environment.
+ * @author <a href="http://www.mxro.de">Max Rohde</a>
+ *
+ */
+package de.mxro.file.Jre;
