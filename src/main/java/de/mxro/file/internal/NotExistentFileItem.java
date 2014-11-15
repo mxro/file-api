115bc941a76a63c4600d57a5ab150e06d926bd0d
mxro <mxro@nowhere.com>
2014-11-15 11:32:28 +1100

    Developing first implementation for FileSync Autocommit Sat Nov 15 11:32:28 EST 2014

--- src/main/java/de/mxro/file/internal/NotExistentFileItem.java
+++ src/main/java/de/mxro/file/internal/NotExistentFileItem.java
@@ -17,92 +17,77 @@ public class NotExistentFileItem implements FileItem {
 
     @Override
     public boolean exists() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public FileItem getChild(final String childName) {
-        // TODO Auto-generated method stub
-        return null;
+        throw new RuntimeException(ILLEGAL_ACCESS);
     }
 
     @Override
     public FileItem assertFolder(final String folderName) {
-        // TODO Auto-generated method stub
-        return null;
+        throw new RuntimeException(ILLEGAL_OPERATION);
     }
 
     @Override
     public FileItem createFile(final String fileName) {
-        // TODO Auto-generated method stub
-        return null;
+        throw new RuntimeException(ILLEGAL_OPERATION);
     }
 
     @Override
     public FileItem deleteFile(final String fileName) {
-        // TODO Auto-generated method stub
-        return null;
+        throw new RuntimeException(ILLEGAL_OPERATION);
     }
 
     @Override
     public FileItem deleteFolder(final String folderName) {
-        // TODO Auto-generated method stub
-        return null;
+        throw new RuntimeException(ILLEGAL_OPERATION);
     }
 
     @Override
     public String getName() {
-        // TODO Auto-generated method stub
-        return null;
+        throw new RuntimeException(ILLEGAL_ACCESS);
     }
 
     @Override
     public String getExtension() {
-        // TODO Auto-generated method stub
-        return null;
+        throw new RuntimeException(ILLEGAL_ACCESS);
     }
 
     @Override
     public FileItem setVisible(final boolean isVisible) {
-        // TODO Auto-generated method stub
-        return null;
+        throw new RuntimeException(ILLEGAL_OPERATION);
     }
 
     @Override
     public boolean getVisible() {
-        // TODO Auto-generated method stub
-        return false;
+        throw new RuntimeException(ILLEGAL_ACCESS);
     }
 
     @Override
     public List<FileItem> getChildren() {
-        // TODO Auto-generated method stub
-        return null;
+        throw new RuntimeException(ILLEGAL_ACCESS);
     }
 
     @Override
     public Date lastModified() {
-        // TODO Auto-generated method stub
-        return null;
+        throw new RuntimeException(ILLEGAL_ACCESS);
     }
 
     @Override
     public String hash() {
-        // TODO Auto-generated method stub
-        return null;
+        throw new RuntimeException(ILLEGAL_ACCESS);
     }
 
     @Override
     public String getText() {
-        // TODO Auto-generated method stub
-        return null;
+        throw new RuntimeException(ILLEGAL_ACCESS);
     }
 
     @Override
     public FileItem setText(final String text) {
-        // TODO Auto-generated method stub
-        return null;
+        throw new RuntimeException(ILLEGAL_OPERATION);
     }
 
 }
